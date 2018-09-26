9a2c2b481e9d9dbe8190e51faec420d65516a2af
ivette <yasminveloz@gmail.com>
2018-09-25 22:09:42 -0500

    created initial app that allows for nested ingredients and steps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require nested_form_fields
 //= require activestorage
 //= require turbolinks
 //= require_tree .
---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ApplicationRecord
+	has_many :ingredients, dependent: :destroy
+
+accepts_nested_attributes_for :ingredients, allow_destroy: true
+
+has_many :steps, dependent: :destroy
+
+accepts_nested_attributes_for :steps, allow_destroy: true
+end
